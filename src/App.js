19163067b8d0b3618d73611865b5b5740c1a1099
naveen-sekar1986 <naveen.sekar1986@gamil.com>
2022-05-21 20:59:18 +0530

    added current location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef} from "react";
+import React,{useState,useRef,useEffect} from "react";
 import axios from "axios";
 import moment from "moment";
 
@@ -13,7 +13,31 @@ function App() {
 
   const inputElement =useRef()
 
+  let intialWeather=(lat,long)=>{
+    console.log('initial weather...')
+  const options = {
+  method: 'GET',
+  url: 'https://yahoo-weather5.p.rapidapi.com/weather',
+  params: {lat: lat, long: long, format: 'json', u: 'f'},
+  headers: {
+    'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com',
+    'X-RapidAPI-Key': 'dcf404886cmsh8d23e88c3f29150p166937jsn55a7d92fd3ab'
+  }
+};
 
+axios.request(options).then(function (response) {
+	console.log(response.data);
+    setError('')
+   fToC(response.data.current_observation.condition.temperature)
+    setcurrentWeather(response.data.current_observation.condition.text)
+    setCity(response.data.location.city)
+    setCountry(response.data.location.country)
+}).catch(function (error) {
+	console.error(error);
+});
+
+
+  }
   let getWeather=()=>{
     console.log('getweahter function.....')
   const input=inputElement.current.value
@@ -64,6 +88,14 @@ function App() {
       console.log(message);
   } 
 
+  useEffect(()=>{
+    console.log('useeffect executed.....')
+    navigator.geolocation.getCurrentPosition(function(position){
+      console.log('Lat',position.coords.latitude)
+      console.log('Long',position.coords.longitude)
+      intialWeather(position.coords.latitude,position.coords.longitude)
+    })
+  })
 
   return (
     <div className={(temperature >16) ? 'app':'app warm'}> 
